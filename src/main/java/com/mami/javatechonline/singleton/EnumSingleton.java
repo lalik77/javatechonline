cbb7ddafbe37e577878e10752d393a7c87709611
Alexander Mami <lalik77@inbox.ru>
2021-02-24 15:35:19 +0300

    Add Joshua Bloch Singlton - Enum Singleton

---
+++ src/main/java/com/mami/javatechonline/singleton/EnumSingleton.java
@@ -0,0 +1,17 @@
+package com.mami.javatechonline.singleton;
+
+
+//Joshua Bloch Singleton
+//using Enum to implement Singleton design pattern ensure that any enum value is instanciated only once .
+//Buy ist inflexible and does not allow lazy instantiation
+public enum
+EnumSingleton {
+
+    INSTANCE;
+
+    public static void doMoreThing(){
+
+
+    }
+
+}
